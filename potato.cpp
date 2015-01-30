d9beca698d397e597a07a40ba7c2089185fc45ce
James <grimscyth@gmail.com>
2015-01-30 10:23:45 -0800

    Fixed bugs relating to pointer and incrementing.

--- potato.cpp
+++ potato.cpp
@@ -1,4 +1,5 @@
 //Potato, by Justin Thomas
+//Fixed pointer errors on line 17 and 32, changed to increment on line 28 - James Rogers
 
 #include <string>
 #include <iostream>
@@ -13,7 +14,7 @@ private:
 public:
 	Potato(string potato)
 	{
-		this.potato = potato;//Needs to use an arrow, not a dot. this references the current instance of Potato, which is a pointer to the current instance.
+		this->potato = potato;//Needs to use an arrow, not a dot. this references the current instance of Potato, which is a pointer to the current instance.
 	}
 	string getPotato(void)
 	{
@@ -24,14 +25,14 @@ public:
 int main(void)
 {
 	Potato* potato = new Potato("potato");
-	for (int i = 0; i < POTATO; i--) // i-- will continually loop this forever. It needs ++ to increment until it reaches potato.
+	for (int i = 0; i < POTATO; i++) // i-- will continually loop this forever. It needs ++ to increment until it reaches potato.
 	{
 		for (int j = i; j < POTATO; j++)
 		{
-			cout << potato.getPotato(); // Needs to use an arrow, not a dot. potato is a pointer to an object.
+			cout << potato->getPotato() << " "; // Needs to use an arrow, not a dot. potato is a pointer to an object.
 		}
 		cout << endl;
 	}
-	system("pause");
+	//system("pause");
 }
 
