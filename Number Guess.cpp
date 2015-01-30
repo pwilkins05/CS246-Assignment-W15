027d2d76e82c112b6d22d37476de8329827bdb3c
Jody <crowsonjj@gmail.com>
2015-01-30 15:58:15 -0800

    I only fixed the issue in number guess where it would not tell you if you got the right number.

--- Number Guess.cpp
+++ Number Guess.cpp
@@ -13,7 +13,7 @@ int main()
 {
     srand (time(NULL));
 
-    int randomNum = rand() % 20; // this is not the correct number to reach 1-20;
+    int randomNum = rand()  % 20; // this is not the correct number to reach 1-20;
     int guess;
     cout << "I will find a random number between 1 and 20" << endl;
     cout << "Try to guess the number and I will tell you if you need to go higher and lower." << endl;
@@ -22,11 +22,11 @@ int main()
     {
         cin >> guess;
 
-        if (guess <= randomNum) //this should not include the correct answer
+        if (guess < randomNum) //this should not include the correct answer
         {
             cout << "You are too low";
         }
-        else if (guess >= randomNum) // this should not include the correct answer
+        else if (guess > randomNum) // this should not include the correct answer
         {
             cout << "You are too high";
         }
