92181d2ce8903629511c2abfeca1e73a097c53cd
Levi Bishop <levi_d_bishop@outlook.com>
2015-01-31 16:11:08 -0800

    Fixed bugs in code

--- Fatal_Error/Fatal_Error.cpp
+++ Fatal_Error/Fatal_Error.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //This program will generate a discount for you based on the starting price you enter
 //
 //Bugs
+/**Fixed by Levi Bishop, Jan 31, 2015*/
 int main()
 {
     double startingPrice, price;
@@ -11,7 +12,7 @@ int main()
     cout << "Enter the price for you item, and we will generate your discount." << endl;
     cin >> startingPrice;
     cout << "Your discount is: $" << calDiscount(startingPrice) << endl;
-    cout << "Your total will be: $" << startingPrice - calDiscount(price) << endl;
+    cout << "Your total will be: $" << startingPrice - calDiscount(startingPrice) << endl;//changed to startingPrice to correct - LB
 
     return 0;
 }
@@ -20,13 +21,13 @@ double calDiscount(double price)
     double disc = 0, discountAmount = 0;
     if(price <= 50)
     {
-        disc = 10;
+        disc = .10;//changed discount from 10 to .10 to correct - LB
         discountAmount = price * disc;
     }
     if(price > 50 && price <= 100)
     {
         disc = .20;
-        discountAmount = price / disc;
+        discountAmount = price * disc;//changed from divide to multiply to correct - LB
     }
     if(price > 100 && price <= 200)
     {
