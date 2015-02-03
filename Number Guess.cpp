e0cf12c71b7974de3ad0a769c4be5d52b17eb8c9
LordKawaii <lordkawaii@gmailc.om>
2015-02-03 11:46:27 -0800

    Fixed "Number Guess.cpp"

--- Number Guess.cpp
+++ Number Guess.cpp
@@ -2,6 +2,8 @@
 // created by James Rogers
 // 01/30/2015
 
+
+//I fixed all the code. Program works as intended.
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
@@ -13,7 +15,9 @@ int main()
 {
     srand (time(NULL));
 
-    int randomNum = rand()  % 20; // this is not the correct number to reach 1-20;
+    //////////////////////Fixed
+    int randomNum = (rand() % 20) + 1; // this is not the correct number to reach 1-20
+
     int guess;
     cout << "I will find a random number between 1 and 20" << endl;
     cout << "Try to guess the number and I will tell you if you need to go higher and lower." << endl;
@@ -22,10 +26,12 @@ int main()
     {
         cin >> guess;
 
+        ////////////////////Dose not include randomNum as an option, only numbers less then it.
         if (guess < randomNum) //this should not include the correct answer
         {
             cout << "You are too low";
         }
+        ////////////////////Dose not include randomNum as an option, only numbers more then it.
         else if (guess > randomNum) // this should not include the correct answer
         {
             cout << "You are too high";
